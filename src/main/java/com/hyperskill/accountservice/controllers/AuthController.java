79b2fc96bcfe2e5a603e31d7037feed7faeac3aa
VhugoJC <vic_360.2000@hotmail.com>
2023-06-16 00:26:39 -0600

    ✨Add exception

--- src/main/java/com/hyperskill/accountservice/controllers/AuthController.java
+++ src/main/java/com/hyperskill/accountservice/controllers/AuthController.java
@@ -1,16 +1,21 @@
 package com.hyperskill.accountservice.controllers;
 
 
-import com.hyperskill.accountservice.exception.ErrorMessage;
 import com.hyperskill.accountservice.models.User;
 import com.hyperskill.accountservice.payloads.UserDTO;
 import com.hyperskill.accountservice.services.UserService;
+
 import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.validation.annotation.Validated;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 
@@ -25,13 +30,7 @@ public class AuthController implements IAuthController {
     @PostMapping("/signup")
     public ResponseEntity<?> addUser(@Validated @RequestBody User newUser){
         if(!this.userService.emailValidation(newUser.getEmail())){ //if email validation fails
-            ErrorMessage errorMsg = new ErrorMessage(
-                    new Date(),
-                    HttpStatus.BAD_REQUEST.value(),
-                    "Bad Request",
-                    "/api/auth/signup"
-            );
-            return new ResponseEntity<>(errorMsg, HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
         UserDTO userResponse = this.modelMapper.map(this.userService.addUser(newUser),UserDTO.class);
         return new ResponseEntity<UserDTO>(userResponse, HttpStatus.OK);
