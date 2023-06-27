79be43e3508f71b0e4b1f26d694526f6803a83a0
VhugoJC <vic_360.2000@hotmail.com>
2023-06-26 18:16:22 -0600

    📝 Add documentation

--- src/main/java/com/hyperskill/accountservice/repositories/ILogRepository.java
+++ src/main/java/com/hyperskill/accountservice/repositories/ILogRepository.java
@@ -2,6 +2,8 @@ package com.hyperskill.accountservice.repositories;
 
 import com.hyperskill.accountservice.models.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ILogRepository extends JpaRepository<Log, Long> {
 }
--- src/main/java/com/hyperskill/accountservice/repositories/IPaymentRepository.java
+++ src/main/java/com/hyperskill/accountservice/repositories/IPaymentRepository.java
@@ -2,9 +2,10 @@ package com.hyperskill.accountservice.repositories;
 
 import com.hyperskill.accountservice.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface IPaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findAllByEmployee(String employee);
 }
